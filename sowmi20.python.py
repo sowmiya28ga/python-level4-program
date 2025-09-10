5fbe7b2cb4714e1a0e380f593c40b625e787e3ca
sowmiya28ga <gasowmiya28@gmail.com>
2025-09-10 15:49:35 +0530

    program20a.py

---
+++ sowmi20.python.py
@@ -0,0 +1,13 @@
+print("Prime Number Checking")
+
+num = int(input("Enter the Number: "))
+
+if num > 1:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is not a prime number")
+            break
+    else:
+        print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
